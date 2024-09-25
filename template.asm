531107f3c635e7dff30bf9261ddd2494ee8719c6
Pranay Beela <pranaybeela@mac.mynetworksettings.com>
2024-09-25 01:08:58 -0400

    commit

---
+++ template.asm
@@ -0,0 +1,20 @@
+TITLE filename
+
+; Header comment block as shown in lecture notes 
+
+INCLUDE Irvine32.inc
+
+.data
+;{ your variables are be defined here}
+
+.code
+main PROC
+
+;{executable code here}
+
+call DumpRegs  ; {you may use this line as many times as necessary to show register/flag values in the code}
+
+exit
+
+main ENDP ; end of main procedure 
+END main ; end of source code
